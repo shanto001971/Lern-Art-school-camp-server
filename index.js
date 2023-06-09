9089f97a6f728b546635e43904eb80a79684405e
Shanto Islam <rajs45693@gmail.com>
2023-06-09 14:22:47 +0600

    implement jwt

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.get('/', (req, res) => {
   res.send('server is running');
 })
 
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ error: true, message: 'unauthorized access' });
+      }
+
+      req.decoded = decoded;
+      next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mi7otul.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,12 +50,19 @@ async function run() {
     const classCollection = client.db("summerSchool").collection("class");
     const mySelectedClass = client.db("summerSchool").collection("myClass");
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+      
+  })
+
     app.get('/class', async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
     })
 
-    app.get('/mySelectedClass', async (req, res) => {
+    app.get('/mySelectedClass',verifyJwt, async (req, res) => {
       const email = req.query.email;
       if (!email) {
           res.send([]);
